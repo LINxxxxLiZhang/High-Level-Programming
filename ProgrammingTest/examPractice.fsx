07d50124a4d794faeac60bf7f750bad1aaed601f
Lizhang Lin <lizhanglin@Lizhangs-MacBook-Pro.local>
2017-02-28 08:08:10 +0000

    added lecture notes and worksheet exercise descriotions

---
+++ ProgrammingTest/examPractice.fsx
@@ -0,0 +1,14 @@
+﻿// Write a function int->int that returns the absolute value of its integer parameter.
+let abs (i:int) :int =
+    if i<0 then -i else i
+
+
+
+// Write a function f: float->float->float such that f a b=sqrt(a*a+b*b)
+let f2 (a:float) (b:float) :float = (a*a+b*b) ** 0.5
+
+
+
+// Write a function threeTimes f (x:int) that computes f(x)^3
+let f (x:int)
+
